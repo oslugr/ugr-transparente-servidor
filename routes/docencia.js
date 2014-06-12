0508054c03e4ac791ab201b9dce55813088e0f1f
jaime <jtbenavente@gmail.com>
2014-06-12 13:23:12 +0200

    añade la oferta y demanda academica

--- routes/docencia.js
+++ routes/docencia.js
@@ -19,14 +19,82 @@
 //Variable para las configuraciones
 var conf = require('../app');
 
+//Variable para la base de datos mongodb
+var MongoClient = require('mongodb').MongoClient;
 
+//Variable para almacenar los datos
+var datos= new Array();
+var datos2= new Array();
+var datos3= new Array();
+
+
+var servidor= new Array();
+var servidor2= new Array();
+var servidor3= new Array();
+
+
+function conectarBD(plantilla,colec,categoria,dataset,v){
+    MongoClient.connect(conf.config.BD, function(err,db){
+          if(err) throw err;
+
+          var coleccion = db.collection(colec);
+
+          if(plantilla==conf.config.ofertaYdemanda.plantilla){
+            datos[v]=new Array();
+            servidor[v]= new Array();
+          }else{
+            datos2[v]=new Array();
+            servidor2[v]= new Array();
+          }
+
+          var cursor = coleccion.find( { categoria: categoria } )
+          cursor.each(function(err, item) {
+                  if(item != null && item.dataset==dataset){
+                    if(plantilla==conf.config.ofertaYdemanda.plantilla)
+                      datos[v].push(item);
+                    else
+                      datos2[v].push(item);
+                  // Si no existen mas item que mostrar, cerramos la conexión con con Mongo y obtenemos los datos 
+                  }
+          });
+
+          var coleccion2 = db.collection(conf.config.datasets);
+          
+          var cursor2 = coleccion2.find()
+          cursor2.each(function(err, item) {
+                  if(item != null){
+                    if(item.dataset==dataset){
+                      if(plantilla==conf.config.ofertaYdemanda.plantilla)
+                        servidor[v].push(item);
+                      else
+                        servidor2[v].push(item);
+                    }
+                  // Si no existen mas item que mostrar, cerramos la conexión con con Mongo y obtenemos los datos 
+                  }else{
+                    db.close();
+                  }
+          });
+
+    });
+}
 
 // Gestión de la pagina de oferta y demanda academica
 
 exports.ofertaYdemanda = function(req, res){
-  var ofYde=conf.config.ofertaYdemanda;
-  res.render(ofYde.plantilla, { seccion: ofYde.nombre , texto: ofYde.texto});
+  var ofyde=conf.config.ofertaYdemanda;
+  for (var i =0;i<(ofyde.dataset).length; i++) {
+    conectarBD(ofyde.plantilla,ofyde.coleccion,ofyde.categoria,ofyde.dataset[i],i);
+  }
+
+  res.render(ofyde.plantilla, { 
+    seccion: ofyde.nombre ,
+    datos: datos,
+    servidores: servidor,
+    tam: (ofyde.dataset).length,
+    contenido: ofyde.contenido
+  });
 };
+
 
 
 // Gestión de la pagina de claustro

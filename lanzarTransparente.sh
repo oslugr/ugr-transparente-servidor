14e2ce548a087f3f567a9a7de8b715943afdc968
Óscar Zafra <oskyar@gmail.com>
2014-09-12 13:36:17 +0200

    se modifica el script de arranque

--- lanzarTransparente.sh
+++ lanzarTransparente.sh
@@ -9,10 +9,10 @@ curl localhost:3000/servicios.html
 curl localhost:3000/infoEconomica.html
 curl localhost:3000/ofertaYdemanda.html
 curl localhost:3000/claustro.html
-curl localhost:3000/alumnos.html
+curl localhost:3000/estudiantes.html
 curl localhost:3000/mision.html
 curl localhost:3000/planEstrategico.html
 curl localhost:3000/gobierno.html
-curl localhost:3000/resultados.html
+curl localhost:3000/estadisticas.html
 curl localhost:3000/leytransparencia.html
 curl localhost:3000/normativa.html

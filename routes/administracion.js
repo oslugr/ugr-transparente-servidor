69becda6d46735be96778363b22c689971322e92
German Martinez <germaaan@gmail.com>
2015-02-19 13:47:55 +0100

    Mongo eliminado de la página Administración/Personal

--- routes/administracion.js
+++ routes/administracion.js
@@ -20,133 +20,45 @@
 //Variable para las configuraciones
 var conf = require('../app');
 
-//Variable para la base de datos mongodb
-var MongoClient = require('mongodb').MongoClient;
-
+// Variable para usar las funciones de la librería Underscore
 var _ = require('underscore');
 
-//Variable para almacenar los datos
-var datos= new Array();
-var datos2= new Array();
-var datos3= new Array();
-
-var servidor= new Array();
-var servidor2= new Array();
-var servidor3= new Array();
-
+// Función para recuperar los datos del JSON
 function getDatos(){
   return conf.configCKAN.personal.datos;
 }
 
+// Función para obtener el tamaño del dataset recuperado del JSON
 function getTamDatos(datos){
   return _.size(datos);
 }
 
+// Función para obtener la dirección del servidor con los datos
 function getServidor(){
   return conf.configCKAN.personal.servidor;
 }
 
-function conectarBD(plantilla,colec,categoria,dataset,v){
-    MongoClient.connect(conf.config.BD, function(err,db){
-          if(err) throw err;
-
-          var coleccion = db.collection(colec);
-
-          if(plantilla==conf.config.personal.plantilla){
-            datos[v]=new Array();
-            servidor[v]= new Array();
-          }else if(plantilla==conf.config.infoEco.plantilla){
-            datos2[v]=new Array();
-            servidor2[v]= new Array();
-          }else{
-            datos3[v]=new Array();
-            servidor3[v]= new Array();
-          }
-
-          var cursor = coleccion.find( { categoria: categoria } )
-          cursor.each(function(err, item) {
-                  if(item != null && item.dataset==dataset){
-                    if(plantilla==conf.config.personal.plantilla)
-                      datos[v].push(item);
-                    else if(plantilla==conf.config.infoEco.plantilla)
-                      datos2[v].push(item);
-                    else
-                      datos3[v].push(item);
-                  // Si no existen mas item que mostrar, cerramos la conexión con con Mongo y obtenemos los datos
-                  }
-          });
-
-          var coleccion2 = db.collection(conf.config.datasets);
-
-          var cursor2 = coleccion2.find()
-          cursor2.each(function(err, item) {
-                  if(item != null){
-                    if(item.dataset==dataset){
-                      if(plantilla==conf.config.personal.plantilla){
-                        servidor[v].push(item);
-                        //console.log(item);
-                        }
-                      else if(plantilla==conf.config.infoEco.plantilla)
-                      servidor2[v].push(item);
-                      else
-                        servidor3[v].push(item);
-                    }
-                  // Si no existen mas item que mostrar, cerramos la conexión con con Mongo y obtenemos los datos
-                  }else{
-                    db.close();
-                  }
-          });
-
-    });
-}
-
 // Gestión de la pagina de personal
-
 exports.personal = function(req, res){
   var personal=conf.config.personal;
-  for (var i =0;i<(personal.dataset).length; i++) {
-    conectarBD(personal.plantilla,personal.coleccion,personal.categoria,personal.dataset[i],i);
-  }
 
-  var ndatos = getDatos();
-  var num_datos = getTamDatos(ndatos);
-  var nservidor = getServidor();
-
-  /*
-  necesarios: personal.plantilla = personal
-              personal.nombre = Personal
-              datos = conf.configCKAN.personal.datos_informacion_salarial
-              servidores = informacion del servidor que no ha gestionado
-              tam = 2 (2 datasets diferentes para esta categoria)
-              contenido = texto sobre las tablas
-  */
-
-
-  //send()
-  //var data = ["informacion-salarial","banda-salarial"];
-
-  //console.log(ndatos[0].descarga);
+  var datos = getDatos();
+  var num_datos = getTamDatos(datos);
+  var servidor = getServidor();
 
   res.render(personal.plantilla, {
     seccion: personal.nombre ,
-    datos: ndatos,
+    datos: datos,
     num_datos: num_datos,
-    servidor: nservidor,
-    //datos: datos,
-    servidores: servidor,
+    servidor: servidor,
     tam: (personal.dataset).length,
     contenido: personal.contenido,
-    //data: data
   });
 };
 
 // Gestión de la pagina de informacion economica
-
 exports.infoEco = function(req, res){
   var infoEco=conf.config.infoEco;
-  for (var i =0;i<(infoEco.dataset).length; i++) {
-    conectarBD(infoEco.plantilla,infoEco.coleccion,infoEco.categoria,infoEco.dataset[i],i);
-  }
 
   res.render(infoEco.plantilla, {
     seccion: infoEco.nombre ,
@@ -158,12 +70,8 @@ exports.infoEco = function(req, res){
 };
 
 // Gestión de la página de servicios
-
 exports.servicios = function(req, res){
   var servicios=conf.config.servicios;
-  for (var i =0;i<(servicios.dataset).length; i++) {
-    conectarBD(servicios.plantilla,servicios.coleccion,servicios.categoria,servicios.dataset[i],i);
-  }
 
   res.render(servicios.plantilla, {
     seccion: servicios.nombre ,
@@ -175,9 +83,7 @@ exports.servicios = function(req, res){
 };
 
 // Gestión de la página de perfil del contratante
-
 exports.perfil = function(req, res){
-
     res.render('perfilContratante', {
         seccion: 'Perfil del Contratante'
     });

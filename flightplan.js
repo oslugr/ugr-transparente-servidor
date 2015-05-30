016b8406761187d498dd5383e627e443b466134d
German Martinez <germaaan@gmail.com>
2015-05-30 21:17:42 +0200

    Despliegue automático funcionando correctamente. Closes #27

--- flightplan.js
+++ flightplan.js
@@ -3,8 +3,7 @@ var plan = require('flightplan');
 
 // Configuración
 plan.target('test', {
-  //host: 'transparente.ugr.es',
-  host: 'testugrts.cloudapp.net',
+  host: 'transparente.ugr.es',
   username: process.env.USER,
   agent: process.env.SSH_AUTH_SOCK
 });
@@ -12,23 +11,21 @@ plan.target('test', {
 // Acciones a ejecutar remotamente
 plan.remote(function(remote) {
   remote.log('Creando copia de seguridad...');
-  remote.exec('sudo cp -Rf ugr-transparente-servidor ugr-transparente-servidor.bak');
+  remote.sudo('cp -Rf ugr-transparente-servidor ugr-transparente-servidor.bak', {user: process.env.USER});
 
   remote.with('cd ugr-transparente-servidor',function() {
     remote.log('Deteniendo el servidor...');
-    remote.exec('npm run-script kill');
+    remote.exec('sudo npm run-script kill');
     remote.log('Restableciendo parámetros de acceso...');
-    //remote.exec('sed "s/IP=transparente.ugr.es/IP=127.0.0.1/" -i package.json');
-    //remote.exec('sed "s/PORT=80/PORT=3000/" -i package.json');
-    remote.exec('sed "s/IP=testugrts.cloudapp.net/IP=127.0.0.1/" -i package.json');
+    remote.exec('sed "s/IP=transparente.ugr.es/IP=127.0.0.1/" -i package.json');
     remote.exec('sed "s/PORT=80/PORT=3000/" -i package.json');
     remote.log('Obteniendo cambios...');
     remote.exec('git pull');
     remote.log('Instalando dependencias...');
     remote.exec('sudo npm install');
     remote.log('Cambiando parámetros de acceso...');
-    //remote.exec('sed "s/IP=127.0.0.1/IP=transparente.ugr.es/" -i package.json');
-    //remote.exec('sed "s/PORT=3000/PORT=80/" -i package.json');
+    remote.exec('sed "s/IP=127.0.0.1/IP=transparente.ugr.es/" -i package.json');
+    remote.exec('sed "s/PORT=3000/PORT=80/" -i package.json');
     remote.log('Arrancando el servidor...');
     remote.exec('sudo npm start');
   });

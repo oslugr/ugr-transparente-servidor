2f7940cff0f3be455dfb6c9e16783a80b290b0c0
German Martinez <germaaan@gmail.com>
2015-11-27 12:54:33 +0100

    Cambia configuración de arranque

--- app.js
+++ app.js
@@ -139,8 +139,8 @@ app.get('/archivos/normativa-legal', function(req, res) {
 });
 
 // Variables de entorno (puerto de escucha y dirección IP)
-app.set('ip', process.env.IP || '0.0.0.0');
-app.set('port', process.env.PORT || 5000 || 3000);
+app.set('port', process.env.PORT);
+app.set('ip', process.env.IP);
 // Directorio con las plantillas
 app.set('views', path.join(__dirname, 'views'));
 // Motor de visualización

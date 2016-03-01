b3d846dde8c03c5527fca57115e4afe2da92c600
German Martinez <germaaan@gmail.com>
2016-03-01 13:54:59 +0100

    Ejecuta xvfb con demonio

--- installChromedriverTravis.sh
+++ installChromedriverTravis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -N http://chromedriver.storage.googleapis.com/2.21/chromedriver_linux64.zip
+wget http://chromedriver.storage.googleapis.com/2.21/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver

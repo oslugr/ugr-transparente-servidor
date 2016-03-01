69807ca39fdf5cf92e3b8bf17d77290ec439d8d9
demiurgosoft <demiurgosoft@hotmail.com>
2016-03-01 10:20:09 +0100

    pequeño arreglo y beautify

--- app/routes.js
+++ app/routes.js
@@ -33,138 +33,121 @@ var administracion = require('./routes/administracion');
 
 
 var config = require('../config/config');
-var jsons=config.archivosJson;
+var jsons = config.archivosJson;
 
 
 //Ejemplo de Rutas configurables
 
 var routesConfig = [{
-    	url: "/personal.html",
-    	plantilla: "personal",
-    	datos: jsons.personal
-    },
-    {
-        url: "/infoEconomica.html",
-        plantilla: "infoEconomica",
-        datos: jsons.infoEconomica
-    },
-    {
-        url: "/ofertaDemanda.html",
-        plantilla: "ofertaDemanda",
-        datos: jsons.ofertaDemanda    
-    },
-    {
-        url: "/claustro.html",
-        plantilla: "claustro",
-        datos: jsons.claustro
-    },
-    {
-        url: "/estudiantes.html",
-        plantilla: "estudiantes",
-        datos: jsons.estudiantes        
-    },
-    {
-        url: "/gobierno.html",
-        plantilla: "gobierno",
-        datos: jsons.gobierno
-    },
-    {
-        url: "/rendimiento.html",
-        plantilla: "rendimiento",
-        datos: jsons.rendimiento
-    },
-    {
-        url:"/normativaLegal.html",
-        plantilla: "normativaLegal",
-        datos: jsons.normativaLegal
-    },
-    {
-        url:"/infoInstitucional.html",
-        plantilla:"infoInstitucional",
-        datos: jsons.infoInstitucional        
-    }
-];
+	url: "/personal.html",
+	plantilla: "personal",
+	datos: jsons.personal
+}, {
+	url: "/infoEconomica.html",
+	plantilla: "infoEconomica",
+	datos: jsons.infoEconomica
+}, {
+	url: "/ofertaDemanda.html",
+	plantilla: "ofertaDemanda",
+	datos: jsons.ofertaDemanda
+}, {
+	url: "/claustro.html",
+	plantilla: "claustro",
+	datos: jsons.claustro
+}, {
+	url: "/estudiantes.html",
+	plantilla: "estudiantes",
+	datos: jsons.estudiantes
+}, {
+	url: "/gobierno.html",
+	plantilla: "gobierno",
+	datos: jsons.gobierno
+}, {
+	url: "/rendimiento.html",
+	plantilla: "rendimiento",
+	datos: jsons.rendimiento
+}, {
+	url: "/normativaLegal.html",
+	plantilla: "normativaLegal",
+	datos: jsons.normativaLegal
+}, {
+	url: "/infoInstitucional.html",
+	plantilla: "infoInstitucional",
+	datos: jsons.infoInstitucional
+}];
 
-var routesArchivos=[
-    {
-        url:'/personal',
-        datos: jsons.personal
-    },
-    {
-        url: '/infoEconomica',
-        datos: jsons.infoEconomica
-    },
-    {
-        url: '/ofertaDemanda',
-        datos: jsons.ofertaDemanda
-    },
-    {
-        url: '/claustro',
-        datos: jsons.claustro        
-    },
-    {
-        url: '/estudiantes',
-        datos: jsons.estudiantes
-    },
-    {
-        url: '/gobierno',
-        datos: jsons.gobierno
-    },
-    {
-        url: '/rendimiento',
-        datos: jsons.rendimiento
-    },
-    {
-        url: '/normativaLegal',
-        datos: jsons.normativaLegal
-    }];
+var routesArchivos = [{
+	url: '/personal',
+	datos: jsons.personal
+}, {
+	url: '/infoEconomica',
+	datos: jsons.infoEconomica
+}, {
+	url: '/ofertaDemanda',
+	datos: jsons.ofertaDemanda
+}, {
+	url: '/claustro',
+	datos: jsons.claustro
+}, {
+	url: '/estudiantes',
+	datos: jsons.estudiantes
+}, {
+	url: '/gobierno',
+	datos: jsons.gobierno
+}, {
+	url: '/rendimiento',
+	datos: jsons.rendimiento
+}, {
+	url: '/normativaLegal',
+	datos: jsons.normativaLegal
+}];
 
 module.exports = function(app) {
 
-    function setRoute(routeConf) {
-        return function(req,res){
-            res.render(routeConf.plantilla, {
-        		servidor: config.servidor,
-        		seccion: routeConf.datos.nombre,
-        		contenido: routeConf.datos.contenido,
-        	});
-        };
-    }
+	function setRoute(routeConf) {
+		return function(req, res) {
+			res.render(routeConf.plantilla, {
+				servidor: config.servidor,
+				seccion: routeConf.datos.nombre,
+				contenido: routeConf.datos.contenido,
+			});
+		};
+	}
 	for (var i = 0; i < routesConfig.length; i++) {
-		app.get(routesConfig[i].url,setRoute(routesConfig[i]));
+		app.get(routesConfig[i].url, setRoute(routesConfig[i]));
 	}
 
 
-    //LEGACY Routes
+	//LEGACY Routes
 	// Inicio
 	app.get('/', index.index);
 	app.get('/index.html', index.index);
-    
-    
-    // Solicitudes de Información
-    app.get('/solicitudInfo.html', solicitudInfo.solicitudInfo);
-    // Mapa del sitio
-    app.get('/mapaWeb.html', mapaWeb.mapaWeb);
-    // Buscador
-    app.get('/buscador.html', buscador.buscador);
-    // Calendario
-    app.get('/calendario.html', calendario.index);
-    app.get('/calendarioSolo.html', calendarioSolo.index);
-    
-    app.get('/perfilContratante.html', administracion.perfil);
 
 
-    
+	// Solicitudes de Información
+	app.get('/solicitudInfo.html', solicitudInfo.solicitudInfo);
+	// Mapa del sitio
+	app.get('/mapaWeb.html', mapaWeb.mapaWeb);
+	// Buscador
+	app.get('/buscador.html', buscador.buscador);
+	// Calendario
+	app.get('/calendario.html', calendario.index);
+	app.get('/calendarioSolo.html', calendarioSolo.index);
+
+	app.get('/perfilContratante.html', administracion.perfil);
+
+
+
 
 	// Archivos de datos
-    function setRouteArchivos(routeConf) {
-        return function(req,res){
-            res.json(routeConf.datos);
-        };
-    }
-    for (var i = 0; i < routesArchivos.length; i++) {
-        app.get('/archivos' + routesArchivos[i].url,setRouteArchivos(routesArchivos[i]));
-    }
+	function setRouteArchivos(routeConf) {
+		return function(req, res) {
+			res.json(routeConf.datos);
+		};
+	}
+	for (i = 0; i < routesArchivos.length; i++) {
+		app.get('/archivos' + routesArchivos[i].url, setRouteArchivos(routesArchivos[i]));
+	}
 
 	// Manejador de errores:
 	app.use(function(req, res, next) {

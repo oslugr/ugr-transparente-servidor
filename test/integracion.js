72bfc05b2f59595c35c45b1d178b0193aea46041
German Martinez <germaaan@gmail.com>
2016-03-02 12:31:37 +0100

    Soluciona problema de Travis

--- test/integracion.js
+++ test/integracion.js
@@ -1,25 +1,25 @@
 var webdriver = require("selenium-webdriver");
 var assert = require("chai").assert;
-var driver;
-
-before(function() {
-  this.timeout(30000);
-  driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
-  return driver.getWindowHandle();
-});
-
-after(function() {
-  return driver.quit();
-});
 
 describe("Prueba de test integración", function() {
-  beforeEach(function() {
-    return driver.get("http://transparente.ugr.es/");
-  });
+	this.timeout(30000);
+	var driver;
 
-  it("Comprueba título", function() {
-    return driver.getTitle().then(function(title) {
-      assert.equal(title, "UGR Transparente | Universidad de Granada");
-    });
-  });
+	before(function() {
+		driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
+	});
+
+	after(function() {
+		return driver.quit();
+	});
+
+	it("Abre página principal", function() {
+		return driver.get("http://transparente.ugr.es/");
+	});
+
+	it("Comprueba título", function() {
+		return driver.getTitle().then(function(title) {
+			assert.equal(title, "UGR Transparente | Universidad de Granada");
+		});
+	});
 });

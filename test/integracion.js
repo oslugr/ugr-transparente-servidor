ddc06beff5982c81cfd45dad39f23cd0c1a4e302
German Martinez <germaaan@gmail.com>
2016-03-02 11:26:08 +0100

    Corrige rendimiento

--- test/integracion.js
+++ test/integracion.js
@@ -1,25 +1,25 @@
 var webdriver = require("selenium-webdriver");
 var assert = require("chai").assert;
+var driver;
+
+before(function() {
+  this.timeout(30000);
+  driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
+  return driver.getWindowHandle();
+});
+
+after(function() {
+  return driver.quit();
+});
 
 describe("Prueba de test integración", function() {
-	this.timeout(30000);
-	var driver;
+  beforeEach(function() {
+    return driver.get("http://transparente.ugr.es/");
+  });
 
-	before(function() {
-		driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
-	});
-
-	after(function() {
-		return driver.quit();
-	});
-
-	it("Abre página principal", function() {
-		return driver.get("http://transparente.ugr.es/");
-	});
-
-	it("Comprueba título", function() {
-		return driver.getTitle().then(function(title) {
-			assert.equal(title, "UGR Transparente | Universidad de Granada");
-		});
-	});
+  it("Comprueba título", function() {
+    return driver.getTitle().then(function(title) {
+      assert.equal(title, "UGR Transparente | Universidad de Granada");
+    });
+  });
 });

78b1e2bc5360a323ff0402ba1cc06bb1fc3193a6
demiurgosoft <demiurgosoft@hotmail.com>
2016-04-05 10:29:58 +0200

    header test

--- test/navegabilidad/navegabilidad.js
+++ test/navegabilidad/navegabilidad.js
@@ -41,13 +41,13 @@ function checkLink(link, done, status) {
 	request(url2).get(dir)
 		.end(function(err, res) {
 			assert.notOk(err);
-			if(res.status>=300 && res.status < 400){
-				var loc=res.header.location;
+			if (res.status >= 300 && res.status < 400) {
+				var loc = res.header.location;
 				assert.ok(loc);
-				return checkLink(loc,done,status);
+				return checkLink(loc, done, status);
 			}
 			if (status) assert.strictEqual(res.status, status);
-			else assert.isBelow(res.status, 400, res.status+" on " + link);
+			else assert.isBelow(res.status, 400, res.status + " on " + link);
 			done();
 		});
 }
@@ -74,6 +74,7 @@ function checkTables(done) {
 		}, cb);
 	}, done);
 }
+
 function checkConnection(title) {
 	title = title || "UGR Transparente | Universidad de Granada";
 	browser.assert.success();
@@ -156,8 +157,18 @@ describe('Pruebas de Navegabilidad', function() {
 		it.skip('Banners', function() {
 			throw (new Error("Not Implemented"));
 		});
-		it.skip('Header', function() {
-			throw (new Error("Not Implemented"));
+		it('Header', function(done) {
+			browser.assert.element('#cabecera');
+			browser.assert.link('#cabecera #enlace_ugr', 'Universidad de Granada', 'http://www.ugr.es/');
+			browser.assert.link('#cabecera #enlace_stack', 'UGR Transparente', '/');
+			browser.assert.link('#cabecera #enlace_eadministracion', 'Administración electrónica', 'http://www.ugr.es/pages/administracion');
+			browser.assert.element('#cabecera #buscador');
+			
+			checkLink('http://www.ugr.es/', function() {
+				checkLink('http://www.ugr.es/pages/administracion', function() {
+					done();
+				});
+			});
 		});
 		it.skip('Footer', function() {
 			throw (new Error("Not Implemented"));
@@ -232,6 +243,9 @@ describe('Pruebas de Navegabilidad', function() {
 			browser.assert.text(elem, 'Normativa Legal');
 			browser.assert.attribute(elem.parentNode, 'href', '/normativaLegal.html');
 			browser.assert.hasClass(elem.parentNode, 'enlaces_index');
+		});
+		it.skip('Dirección superior',function(){
+			
 		});
 	});
 	describe('Información Institucional', function() {
@@ -521,15 +535,25 @@ describe('Pruebas de Navegabilidad', function() {
 			browser.assert.link('#pagina a', 'Acceso al Formulario', 'https://sede.ugr.es/sede/catalogo-de-procedimientos/solicitud-generica.html');
 			//https without certificate!
 			//checkLink('https://sede.ugr.es/sede/catalogo-de-procedimientos/solicitud-generica.html', done);
-			browser.assert.link('#pagina a','Ley 19/2013, de 9 de diciembre, de transparencia, acceso a la información pública y buen gobierno.','http://www.boe.es/boe/dias/2013/12/10/pdfs/BOE-A-2013-12887.pdf');
-			checkLink('http://www.boe.es/boe/dias/2013/12/10/pdfs/BOE-A-2013-12887.pdf',done);
+			browser.assert.link('#pagina a', 'Ley 19/2013, de 9 de diciembre, de transparencia, acceso a la información pública y buen gobierno.', 'http://www.boe.es/boe/dias/2013/12/10/pdfs/BOE-A-2013-12887.pdf');
+			checkLink('http://www.boe.es/boe/dias/2013/12/10/pdfs/BOE-A-2013-12887.pdf', done);
 		});
 	});
 	describe.skip('Buscador', function() {
+		before(function(done) {
+			browser.visit(url + '/', function(err) {
+				assert.notOk(err);
+				done();
+			});
+		});
+		it.skip('Búsqueda', function() {
 
+		});
+		it.skip('Layout', function() {
 
+		});
 	});
-	describe('404 page',function(){
+	describe('404 page', function() {
 		before(function(done) {
 			browser.visit(url + '/foo', function(err) {
 				done();

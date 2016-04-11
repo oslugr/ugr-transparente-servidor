938dcb1999dec1d8a923e9b3c8f423283ac8d841
demiurgosoft <demiurgosoft@hotmail.com>
2016-04-11 11:32:40 +0200

    csv descargado y procesado por d3.js #53

--- public/script/generar_tabla.js
+++ public/script/generar_tabla.js
@@ -1,6 +1,7 @@
 /*
 	UGR Transparente. Sitio Web de la Universidad de Granada de acceso a Datos Abiertos.
 	Copyright (C) 2014 Mario Heredia Moreno, Jaime Torres Benavente
+    Copyright (C) 2016 Andrés Ortiz Corrales
 
 	This file is part of UGR Transparente.
 
@@ -19,162 +20,92 @@
 */
 
 tabla(document).ready(function() {
-	// Select all the a tag with name equal to modal
-	tabla('a[class=view]').click(function(e) {
-		// Cancel the link behavior
-		e.preventDefault();
-		// Get the A tag
+    // Select all the a tag with name equal to modal
+    tabla('a[class=view]').click(function(e) {
+        // Cancel the link behavior
+        e.preventDefault();
 
-		var id = tabla(this).attr('href');
+        var id = tabla(this).attr('href');
 
-		// Get the screen height and width
-		var maskHeight = tabla(document).height();
-		var maskWidth = tabla(window).width();
+        // Get the screen height and width
+        var maskHeight = tabla(document).height();
+        var maskWidth = tabla(window).width();
 
-		// Set height and width to mask to fill up the whole screen
-		tabla('#mask').css({
-			'width': maskWidth,
-			'height': maskHeight
-		});
+        // Set height and width to mask to fill up the whole screen
+        tabla('#mask').css({
+            'width': maskWidth,
+            'height': maskHeight
+        });
 
-		// Transition effect
-		tabla('#mask').fadeIn(10);
-		tabla('#mask').fadeTo("slow", 0.8);
+        // Transition effect
+        tabla('#mask').fadeIn(10);
+        tabla('#mask').fadeTo("slow", 0.8);
 
-		// Get the window height and width
-		var winH = tabla(window).height();
-		var winW = tabla(window).width();
+        // Get the window height and width
+        var winH = tabla(window).height();
+        var winW = tabla(window).width();
 
-		// Set the popup window to center
-		tabla(id).css('top', winH / 2 - tabla(id).height() / 2);
-		tabla(id).css('left', winW / 2 - tabla(id).width() / 2);
+        // Set the popup window to center
+        tabla(id).css('top', winH / 2 - tabla(id).height() / 2);
+        tabla(id).css('left', winW / 2 - tabla(id).width() / 2);
 
-		// Transition effect
-		tabla(id).fadeIn(2000);
+        // Transition effect
+        tabla(id).fadeIn(2000);
 
-		var nombre = tabla(this).attr('name');
+        var nombre = tabla(this).attr('name');
 
-		tabla('#titulo_tabla').html(nombre);
+        tabla('#titulo_tabla').html(nombre);
 
-		tabla.ajaxSetup({
-			'beforeSend': function(xhr) {
-				xhr.overrideMimeType('text/html; charset=ISO-8859-1');
-			}
-		});
+        drawTable(tabla(this).attr("rel")); //use tabla.csv.toArrays(data)  to parse it as array
+        
+    /*    tabla.ajaxSetup({
+            'beforeSend': function(xhr) {
+                xhr.overrideMimeType('text/html; charset=ISO-8859-1');
+            }
+        });
+        var url=tabla(this).attr("rel");
+        tabla.ajax({
+            type: "GET",
+            url: tabla(this).attr("rel"),
+            dataType: "text",
+            success: function(data) {
+                drawTable(data,url); //use tabla.csv.toArrays(data)  to parse it as array
+            }
+        });*/
 
-		tabla.ajax({
-			type: "GET",
-			url: tabla(this).attr("rel"),
-			dataType: "text",
-			success: function(data) {
-				drawTable(tabla.csv.toArrays(data));
-			}
-		});
+    });
 
-	});
+    // If close button is clicked
+    tabla('#close').click(function(e) {
+        tabla('#mask, .window').hide();
+        tabla('#mask, .window').HTML("");
+    });
 
-	// If close button is clicked
-	tabla('#close').click(function(e) {
-		tabla('#mask, .window').hide();
-		tabla('#mask, .window').HTML("");
-	});
-
-	// If mask is clicked
-	tabla('#mask').click(function() {
-		tabla(this).hide();
-		tabla('.window').hide();
-		tabla(this).HTML("");
-		tabla('.window').HTML("");
-	});
+    // If mask is clicked
+    tabla('#mask').click(function() {
+        tabla(this).hide();
+        tabla('.window').hide();
+        tabla(this).HTML("");
+        tabla('.window').HTML("");
+    });
 
 });
 
-google.load('visualization', '1', {
-	packages: ['table']
-});
+function drawTable(url) {
+    $('#tabla').empty();
+    d3.text(url,"text/plain; charset=ISO-8859-1", function(data) {
+        console.log(data);
+                    var parsedCSV = d3.csv.parseRows(data);
+                    var container = d3.select("#tabla")
+                        .append("table")
 
-function isNumber(cell) {
-	var i = 0;
-	var cont = true;
+                        .selectAll("tr")
+                            .data(parsedCSV).enter()
+                            .append("tr")
 
-	if (cell.charAt(0) == '-') {
-		i++;
-	}
-
-	if (cell.charAt(0) == '0') {
-		if (cell.charAt(1) != ',' && cell.charAt(1) !== '') {
-			cont = false;
-		}
-	}
-
-	while (cont && i < cell.length) {
-		switch (cell.charAt(i)) {
-			case '0':
-				i++;
-				break;
-			case '1':
-				i++;
-				break;
-			case '2':
-				i++;
-				break;
-			case '3':
-				i++;
-				break;
-			case '4':
-				i++;
-				break;
-			case '5':
-				i++;
-				break;
-			case '6':
-				i++;
-				break;
-			case '7':
-				i++;
-				break;
-			case '8':
-				i++;
-				break;
-			case '9':
-				i++;
-				break;
-			case ',':
-				i++;
-				break;
-			default:
-				cont = false;
-		}
-	}
-
-	return cont;
-}
-
-function drawTable(csv) {
-	var data = new google.visualization.DataTable();
-	var table = new google.visualization.Table(document.getElementById('tabla'));
-	var i, j, a;
-
-	for (i = 0; i < csv[0].length; i++) {
-		data.addColumn('string', csv[0][i]);
-	}
-
-	data.addRows(csv.length - 1);
-
-	for (i = 1; i < csv.length; i++) {
-		for (j = 0; j < csv[0].length; j++) {
-			if (isNumber(csv[i][j])) {
-				data.setCell(i - 1, j, csv[i][j], csv[i][j], {
-					style: 'text-align: right'
-				});
-			} else {
-				data.setCell(i - 1, j, csv[i][j]);
-			}
-		}
-	}
-
-	table.draw(data, {
-		allowHtml: true,
-		showRowNumber: true
-	});
+                        .selectAll("td")
+                            .data(function(d) { return d; }).enter()
+                            .append("td")
+                            .text(function(d) { return d; });
+                });
 }

8639f03effd2c02c6c86a6813d31f947a7bce04c
German Martinez <germaaan@gmail.com>
2016-04-28 10:23:28 +0200

    Cambios en archivos

--- test/navegabilidad/navegabilidad.js
+++ test/navegabilidad/navegabilidad.js
@@ -651,7 +651,7 @@ describe('Pruebas de Navegabilidad', function() {
 				}, 5 * 1000);
 			});
 		});
-		it.skip('Links',function(done){
+		it.skip('Links', function(done) {
 			return done(new Error("not implemented"));
 		});
 		it('Menú de Rastro', function() {
